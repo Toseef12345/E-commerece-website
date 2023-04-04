3e0f07cb6485f39946348d6c789b1ed6be5a8314
Toseef Dar <toseefdar534@gmail.com>
2023-04-04 08:08:39 +0500

    api integretion of seller-signup

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FormGroupDirective } from '@angular/forms';
+import { SellerService } from '../services/seller.service';
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.scss']
 })
-export class SellerAuthComponent implements OnInit{
+export class SellerAuthComponent implements OnInit {
   sellerForm!: FormGroup<any>;
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb: FormBuilder, private sellerService: SellerService) { }
   ngOnInit(): void {
-  this.sellerForm=this.fb.group({
-    name:[''],
-    email:[],
-    phone:[]
-  })
+    this.sellerForm = this.fb.group({
+      name: [''],
+      email: [],
+      phone: []
+    })
   }
   signUp(): void {
-    console.log(this.sellerForm.value);
+    this.sellerService.postSellerFormCredentials(this.sellerForm.value).subscribe(res => {
+      console.log("data from backend",res)
+    }
+    )
   }
 }
