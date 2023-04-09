4d55914a559427e0ad2c9d13f264ea0d68db3296
Toseef Dar <toseefdar534@gmail.com>
2023-04-09 15:15:44 +0500

    implement a logic in which navebar will be changed for user and admin/seller

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  whoIsLoggedIn: String = 'simple';
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+    this.router.events.subscribe((val: any) => {
+      if (val.url) {
+        if (localStorage.getItem('sellerLogin') && val.url.includes('seller')) {
+          this.whoIsLoggedIn = 'seller'
+          console.warn('inside seller')
+        }
+        else{
+          console.warn("outside seller");
+          
+        }
+      }
+
+    })
+  }
 
 }
