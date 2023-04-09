fc62226ecf2a9a7829c3b606d8b034a1803fb949
Toseef Dar <toseefdar534@gmail.com>
2023-04-09 12:38:54 +0500

    api integration of login and using auth guard for telling route to stay activate until user logout

--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,32 +1,61 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { SellerInterface } from '../interfaces/seller-credentials';
 import { Router } from '@angular/router';
+import { SellerLoginInterface } from '../interfaces/seller-login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SellerService {
+  loginError: boolean = false;
+
+
   isUserLoggedIn = new BehaviorSubject<boolean>(false)
-  constructor(private http: HttpClient,private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
 
-  postSellerFormCredentials(data: SellerInterface): void {
-   this.http.post<SellerInterface[]>('http://localhost:3000/seller', data).subscribe(res => {
-      if (res) {
-        localStorage.setItem('seller',JSON.stringify(res))
-        this.isUserLoggedIn.next(true)
-        this.router.navigate(['seller-dashboard'])
-      }
-      
-    })
+  public userSignup(data: SellerInterface): void {
+    this.http.post<SellerInterface[]>('http://localhost:3000/seller', data)
+      .subscribe(res => {
+        if (res) {
+          localStorage.setItem('seller', JSON.stringify(res))
+          this.isUserLoggedIn.next(true)
+          this.router.navigate(['seller-dashboard'])
+        }
+
+      })
   }
-  sellerReload(){
-    if(localStorage.getItem('seller')){
+  public sellerReload(): void {
+    if (localStorage.getItem('seller')) {
       this.isUserLoggedIn.next(true)
       this.router.navigate(['seller-dashboard'])
 
+    }
+  }
+  public userLogin(data: SellerLoginInterface): void {
+    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`)
+      .subscribe((res: any) => {
+        if (res.length >= 1) {
+          console.log("Login Successfull");
+          localStorage.setItem('sellerLogin', JSON.stringify(res))
+          this.loginError = false
+          this.isUserLoggedIn.next(true)
+          this.router.navigate(['seller-dashboard'])
+        }
+        else {
+          this.loginError = true
+          console.log("Please Enter Valid Detail");
+        }
+
+
+      })
+  }
+  public sellerLoginReload(): void {
+    if (localStorage.getItem('sellerLogin')) {
+      this.isUserLoggedIn.next(true)
+      this.router.navigate(['seller-dashboard'])
     }
   }
 }
